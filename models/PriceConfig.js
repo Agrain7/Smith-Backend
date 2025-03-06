016380acf16fe95f8833ae5d657a9af0f9d372d6
Agrain7 <cgmaeng@ipil.co.kr>
2025-03-06 09:32:29 +0900

    가격표 수정사항 업로드2

--- models/PriceConfig.js
+++ models/PriceConfig.js
@@ -2,12 +2,26 @@
 const mongoose = require('mongoose');
 
 const priceConfigSchema = new mongoose.Schema({
-  sm275: { type: Number, default: 1000 },
-  sm355: { type: Number, default: 1200 },
+  비규격: {
+    "9t이하": { type: Number, default: 800 },
+    "12~50t": { type: Number, default: 1000 }
+  },
+  중국산: {
+    "9t이하": { type: Number, default: 900 },
+    "12~50t": { type: Number, default: 1100 }
+  },
+  SM275: {
+    "9t이하": { type: Number, default: 1000 },
+    "12~50t": { type: Number, default: 1200 }
+  },
+  SM355: {
+    "9t이하": { type: Number, default: 1100 },
+    "12~50t": { type: Number, default: 1300 }
+  },
   processingFee: {
     현장용소부재: { type: Number, default: 199 },
     공장용소부재: { type: Number, default: 188 },
-    브라켓: { type: Number, default: 177 },
+    브라켓: { type: Number, default: 177 }
   }
 }, { timestamps: true });
 
