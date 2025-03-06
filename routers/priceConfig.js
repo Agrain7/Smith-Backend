3dcae9679d831bdedd5ebc15045797b77c39d83d
Agrain7 <cgmaeng@ipil.co.kr>
2025-03-06 09:29:29 +0900

    가격표 수정사항 업로드

--- routers/priceConfig.js
+++ routers/priceConfig.js
@@ -7,7 +7,6 @@ const PriceConfig = require('../models/PriceConfig');
 router.get('/price-config', async (req, res) => {
   try {
     let config = await PriceConfig.findOne({});
-    // 만약 저장된 설정이 없다면 기본값을 생성
     if (!config) {
       config = await PriceConfig.create({});
     }
@@ -20,24 +19,25 @@ router.get('/price-config', async (req, res) => {
 
 // PUT /api/price-config : 가격 설정 업데이트
 router.put('/price-config', async (req, res) => {
-    try {
-      console.log('PUT /api/price-config req.body:', req.body); // 추가
-      const { sm275, sm355, processingFee } = req.body;
-      let config = await PriceConfig.findOne({});
-      if (!config) {
-        config = await PriceConfig.create({ sm275, sm355, processingFee });
-      } else {
-        config.sm275 = sm275;
-        config.sm355 = sm355;
-        config.processingFee = { ...config.processingFee, ...processingFee };
-        await config.save();
-      }
-      res.json({ success: true, config });
-    } catch (err) {
-      console.error("가격 설정 업데이트 오류:", err);
-      res.status(500).json({ success: false, message: "서버 오류 발생", error: err.message });
+  try {
+    console.log('PUT /api/price-config req.body:', req.body);
+    const { 비규격, 중국산, SM275, SM355, processingFee } = req.body;
+    let config = await PriceConfig.findOne({});
+    if (!config) {
+      config = await PriceConfig.create({ 비규격, 중국산, SM275, SM355, processingFee });
+    } else {
+      config.비규격 = 비규격;
+      config.중국산 = 중국산;
+      config.SM275 = SM275;
+      config.SM355 = SM355;
+      config.processingFee = { ...config.processingFee, ...processingFee };
+      await config.save();
     }
-  });
-  
+    res.json({ success: true, config });
+  } catch (err) {
+    console.error("가격 설정 업데이트 오류:", err);
+    res.status(500).json({ success: false, message: "서버 오류 발생", error: err.message });
+  }
+});
 
 module.exports = router;
